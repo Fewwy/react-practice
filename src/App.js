fcb686b98b2629e6b1e881595d39a463dfe6ae2d
Aleksandr Voznesenskii <62722417+Fewwy@users.noreply.github.com>
2022-07-08 16:45:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import GalleryPage from './components/GalleryPage/GalleryPage';
 import Menu from './components/Menu/Menu';
-import { Link } from "react-router-dom";
 import './app.css';
 import MainPage from './components/MainPage/MainPage';
 
