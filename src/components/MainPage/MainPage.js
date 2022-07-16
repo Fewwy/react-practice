8486d775c9036cd23459b4928a709725e2626438
Aleksandr Voznesenskii <avoznese@redhat.com>
2022-07-16 17:21:46 +0200

    updating CI settings

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -43,18 +43,18 @@ export default function MainPage() {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <a
-                      href="#"
+                      href="https://www.fiverr.com/polinka_dot?source=gig_page&gigs=slug%3Adraw-you-a-portrait-in-original-style%2Cpckg_id%3A1"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                     >
-                      Get started
+                      Fiverr Page
                     </a>
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
-                      href="#"
+                      href="https://www.fiverr.com/polinka_dot?source=gig_page&gigs=slug%3Adraw-you-a-portrait-in-original-style%2Cpckg_id%3A1"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                     >
-                      Live demo
+                      Fiverr Page
                     </a>
                   </div>
                 </div>
