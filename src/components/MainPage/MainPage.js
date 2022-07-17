1b310091f4211e8fe203a5e240d8a7269b5b47c7
Aleksandr Voznesenskii <avoznese@redhat.com>
2022-07-17 15:50:35 +0200

    Removed profile button

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Popover} from '@headlessui/react'
+import mainPageImage from '../../assets/mainPageImage.jpg'
 
 export default function MainPage() {
     return (
-    <div className="max-h-full max-h-screen min-h: 80vh">
+    <div className="max-h-full max-h-screen min-h: ">
       <div className="relative bg-white overflow-hidden ">
         <div className="max-w-7xl mx-auto">
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 text-center">
@@ -65,7 +66,7 @@ export default function MainPage() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className=""
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            src={mainPageImage}
             alt=""
           />
         </div>
